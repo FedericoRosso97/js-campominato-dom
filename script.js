c1cf80b6fffaf9ada5b1f80245fab539f1b1231c
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-24 12:34:23 +0200

    copio cartelljs/css/html

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const button=document.querySelector('button')
+const main=document.querySelector('main');
+
+button.addEventListener('click',function(){
+    main.innerHTML='';
+ /*creo le 3 diverse difficoltà */
+    let Input=document.querySelector('select');
+    let difficulty=Input.value;
+    let durata;
+    
+    if(difficulty=='facile'){
+        durata=49;
+        squareClass='box3';
+       }else if(difficulty=='media'){
+           durata=81;
+           squareClass='box2';
+       }else if(difficulty=='difficile'){
+           durata=100;
+           squareClass='box';
+       }
+       console.log(difficulty);
+
+ /*creo griglia e box*/
+const griglia=creaElemento('div','grid');
+
+main.append(griglia);
+
+let number=0;
+for(let i=0;i<durata;i++){
+    let square=creaElemento('div',squareClass);
+    
+       griglia.append(square);
+    
+       square.addEventListener('click',function(){
+       
+        square.classList.toggle('blu');
+        console.log(i+1);
+
+    });    
+    
+    number=number+1;
+     square.append(number);
+}
+})
+
+
+/*funzioni */
+function creaElemento(elementi,classe){
+    let myElement=document.createElement(elementi);
+    myElement.classList.add(classe);
+
+    return myElement;
+}
+
+   
