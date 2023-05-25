b80eccfea2a6ed7a095f1b928270c1a6e73947da
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-25 15:04:07 +0200

    termino partita

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-gino=Math.floor(0.234);
-console.log(gino);
+
 const button=document.querySelector('button')
 const main=document.querySelector('main');
 
 button.addEventListener('click',function(){
     main.innerHTML='';
+    let inGame=true;
  /*creo le 3 diverse difficoltà */
     const Input=document.querySelector('select');
     let difficulty=Input.value;
@@ -13,15 +13,15 @@ button.addEventListener('click',function(){
     if(difficulty=='facile'){
         durata=49;
         squareClass='box3';
-        bombe=9;
+        bombe=4;
        }else if(difficulty=='media'){
            durata=81;
            squareClass='box2';
-           bombe=10;
+           bombe=8;
        }else if(difficulty=='difficile'){
            durata=100;
            squareClass='box';
-           bombe=16;
+           bombe=12;
        }
        console.log(difficulty);
 
@@ -33,27 +33,38 @@ main.append(griglia);
 let number=0;
 let bomba=randomUnicNumber(1,durata,bombe);
 console.log(bomba)   
+
 for(let i=0;i<durata;i++){
     let square=creaElemento('div',squareClass);
+       
+    griglia.append(square);
+    let indice=i+1; 
+     square.addEventListener('click',function(){
     
-       griglia.append(square);
-     finePartita=false;
-     if(finePartita=false){ square.addEventListener('click',function(){
-        square.classList.toggle('blu');
-        
-        let indice=i+1;
-        console.log(indice);
-
-        if(bomba.includes(indice)){
-            square.classList.toggle('red');
-            square.classList.remove('blu');
-            finePartita=true;
-        }  
-    });    
-    number=number+1;
-    square.append(number);
-}
-}})
+        if(inGame){
+            if(bomba.includes(indice)){
+                square.classList.add('red');
+                square.classList.remove('blu');
+                inGame=false;
+                console.log(inGame);
+               }else{
+                
+                square.classList.add('blu'); 
+                 inGame=true;
+                console.log(inGame);
+               }
+        }else{
+            //alert('GAME OVER!,non puoi più cliccare')
+            console.log('game over!')
+        }
+         console.log(indice);
+    }); 
+    //number=number+1;
+    //square.append(number);
+   
+ }
+ 
+})
 
       
 
